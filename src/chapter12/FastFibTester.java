49536e5d6e3b6b87e4f94f80a60782fc54d62c39
Eric <ejyu99@gmail.com>
2019-03-29 10:42:59 -0600

    Added FastFib Assignment

--- src/chapter12/FastFibTester.java
+++ src/chapter12/FastFibTester.java
@@ -11,13 +11,19 @@ public class FastFibTester {
         System.out.println("Please enter the fibonacci length n: ");
         n = in.nextInt();
 
-        long[] numList = new long[n];
+        long[] fibList = new long[n];
 
-        for (int i = 1; i <= n; i++) {
+        double prevTime = System.currentTimeMillis();
+        System.out.println(fib.slowFibonacci(n));
+        double postTime = System.currentTimeMillis();
 
-            long f = fib.fibonacci(i, numList);
-            System.out.println("fib(" + i + ") = " + f);
-        }
+        System.out.println("The running time for the slower fibonacci: " + (postTime - prevTime));
+
+        prevTime = System.currentTimeMillis();
+        System.out.println(fib.fastFibonacci(n, fibList));
+        postTime = System.currentTimeMillis();
+
+        System.out.println("The running time for the faster fibonacci: " + (postTime - prevTime));
 
     }
 }
